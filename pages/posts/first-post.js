ac6bb4fd57014fde871056cbaabce9e3e845692f
nelsonic <nelson+github@dwyl.com>
2020-10-25 10:34:21 +0000

    create layout component and css module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,17 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+    <head><title>First Post</title></head>
     <h1>First Post</h1>
     <h2>
       <Link href="/">
       <a>Back to home</a>
       </Link>
     </h2>
-    </>
+    </Layout>
   )
 }
