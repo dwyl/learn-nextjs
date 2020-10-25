d580f11bf8e615a39888995b4f84141880ce34eb
nelsonic <nelson+github@dwyl.com>
2020-10-25 13:51:21 +0000

    implement blog rendering with getStaticProps

--- README.md
+++ README.md
@@ -23,3 +23,19 @@ This isn't exactly _light_ on dependencies:
 Installing packages. This might take a couple of minutes.
 added 694 packages from 283 contributors
 ```
+
+Installed `gray-matter`:
+
+```
+npm install gray-matter --save-dev
++ gray-matter@4.0.2
+added 8 packages from 18 contributors and audited 702 packages in 4.221s
+
+34 packages are looking for funding
+  run `npm fund` for details
+
+found 5 vulnerabilities (3 low, 1 moderate, 1 high)
+  run `npm audit fix` to fix them, or `npm audit` for details
+```
+
+**1 high** ... unsurprising in **702** packages ... 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostData = getSortedPostsData()
+  return {
+    props: {
+      allPostData
+    }
+  }
+}
+
+
+export default function Home({ allPostData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +25,20 @@ export default function Home() {
           (sample website - built following{' '}
           <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostData.map(({id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
